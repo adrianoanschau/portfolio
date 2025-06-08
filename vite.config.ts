6cd2e83ff8777d3144c0245cc45f487b3ba1bbd2
Adriano Anschau <adrianoanschau@gmail.com>
2025-06-08 12:59:15 -0300

    Initialize React app with Vite, TypeScript, ESLint, Prettier, and project configurations.

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig(() => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all envs regardless of the `VITE_` prefix.
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    build: {
+      sourcemap: true,
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: ['./src/test/setup.ts'],
+    },
+  };
+});
